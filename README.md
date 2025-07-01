a43e9c4c412dbaf95326af2e4849fded32c428e4
thehxdev <hossein.khosravi.ce@gmail.com>
2025-07-02 00:02:21 +0330

    updated the example program and docs
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is an implementation of [Huffman Coding](https://en.wikipedia.org/wiki/Huffman_coding) algorithm in C.
 
 ## Get the source code
-This project depends on [sth](https://github.com/thehxdev/sth) library which is a base layer or my C projects.
+This project depends on [sth](https://github.com/thehxdev/sth) library which is a base layer for my C projects.
 So you must clone repository with submodules.
 ```bash
 git clone --recurse-submodules --remote-submodules \
@@ -13,7 +13,12 @@ git clone --recurse-submodules --remote-submodules \
 Build the project with `cmake`.
 ```bash
 mkdir -p build
-cmake -B build -S .
+cmake -DCMAKE_BUILD_TYPE=Release -B build -S .
 cmake --build build
 ```
-This will build the example executable that builds a Huffman tree and prints it.
+This will build the example executable that accepts a path to a file from command line and
+generates a Huffman Tree. Then prints the symbols, their frequency and encoded binary code.
+```bash
+cd build
+./hc '<path_to_a_file>'
+```
--- main.c
+++ main.c
@@ -7,39 +7,71 @@
 #include "malloc.h"
 #include "huffman_code.h"
 
+#define BUFFER_SIZE (1024 * 1024)
+
 // These example inputs copy-pasted from wikipedia
 // static const hc_byte_t input[] = "this is an example of a huffman tree";
-static const hc_byte_t input[] = "A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED";
+// static const hc_byte_t input[] = "A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED";
 // static const hc_byte_t input[] = "";
 
-int main(void) {
+int main(int argc, char *argv[]) {
+	int err = 0;
+	FILE *fp = NULL;
+	hc_ctx_t ctx;
+	size_t nread;
+	unsigned char *buffer = NULL;
+
+	if (argc != 2) {
+		printf("Usage: %s <file-path>\n", argv[0]);
+		exit(1);
+	}
+
 	// use sth memory debugger and allocation tracer to find memory bugs
 	// and leaks.
 	sth_mem_dbg_init();
 
-	int err;
-	hc_ctx_t ctx;
+	if ( (err = hc_ctx_init(&ctx))) {
+		fprintf(stderr, "failed to create huffman context\n");
+		goto ret;
+	}
 
-	const size_t input_size = sizeof(input)-1;
+	fp = fopen(argv[1], "rb");
+	if (!fp) {
+		perror("fopen");
+		goto destroy_ctx_ret;
+	}
 
-	err = hc_ctx_init(&ctx);
-	assert(err == 0);
+	buffer = malloc(BUFFER_SIZE);
+	if (!buffer) {
+		fprintf(stderr, "failed to allocate memory for buffer\n");
+		goto close_fp_ret;
+	}
 
-	hc_ctx_freqs_update(&ctx, input, input_size);
-	err = hc_ctx_freqs_finalize(&ctx);
-	assert(err == 0);
+	while ( (nread = fread(buffer, sizeof(*buffer), BUFFER_SIZE, fp)) > 0)
+		hc_ctx_freqs_update(&ctx, buffer, nread);
 
-	err = hc_ctx_tree_build(&ctx);
-	assert(err == 0);
+	if ( (err = hc_ctx_freqs_finalize(&ctx)))
+		goto free_buffer_ret;
+
+	if ( (err = hc_ctx_tree_build(&ctx)))
+		goto free_buffer_ret;
 
 	hc_ctx_tree_print(ctx.root);
 
+	// Just a trap for debugging
 	// puts("trap!");
 	// *(volatile char*)0;
 
+// Defer-like return
+free_buffer_ret:
+	free(buffer);
+close_fp_ret:
+	if (fp)
+		fclose(fp);
+destroy_ctx_ret:
 	hc_ctx_destroy(&ctx);
-
+ret:
 	sth_mem_dbg_print();
 	sth_mem_dbg_destroy();
-	return 0;
+	return err;
 }
